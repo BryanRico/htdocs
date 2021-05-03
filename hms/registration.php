e8331b0c941ccd9b07de2a9a80a2a9e2c1294a8b
BryanRico <bryan.rc597@gmail.com>
2021-05-03 14:24:42 -0500

    Se agrega Icono en las pestañas

--- hms/registration.php
+++ hms/registration.php
@@ -29,6 +29,7 @@ if($query)
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<meta content="" name="description" />
 		<meta content="" name="author" />
+		<link rel="shortcut icon" href="images/SD2Ico.ico" >
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
