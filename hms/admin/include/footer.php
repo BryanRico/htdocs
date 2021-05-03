777b1da1518b415762a5a142b5fe9ead1a37e8e2
BryanRico <bryan.rc597@gmail.com>
2021-05-03 13:26:08 -0500

    Se agrega la carptea hms

---
+++ hms/admin/include/footer.php
@@ -0,0 +1,11 @@
+<footer>
+	<meta charset="utf-8">
+				<div class="footer-inner">
+					<div class="pull-left">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> SaveDoctor</span>. <span>Todos los derechos reservados</span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
