afd0cdde1b326813b4efbb9ba6187cd4e4c2dd60
MiguelM01 <miky_sk5@hotmail.com>
2021-05-03 20:23:56 -0500

    Cambio campos alta doctor
    Se da un cambio en los campos de alta del doctor y especialización.

--- hms/admin/doctor-specilization.php
+++ hms/admin/doctor-specilization.php
@@ -88,7 +88,7 @@ if(isset($_GET['del']))
 															<label for="exampleInputEmail1">
 																Especialización médica
 															</label>
-							<input type="text" name="doctorspecilization" class="form-control"  placeholder="Enter Doctor Specialization">
+							<input type="text" name="doctorspecilization" class="form-control"  placeholder="Ingresar especialización del doctor">
 														</div>
 												
 														
